86fe64a01a491e75fe1b3b7a9cd8970642d5e14a
Warren Lazarraga <79947453+WarrenLaz@users.noreply.github.com>
2022-02-16 15:24:58 -0500

    Bug Fix: Duplicates
    else if (i==j && this.WordofDay[i] == this.myWord[j] ) {
    					break;
    				}
    
    if the indexes are the same and the letters are the same in this index it already is in the right spot. So, # is not printed and loop breaks.

--- CheckWord.java
+++ CheckWord.java
@@ -10,17 +10,15 @@ public class CheckWord {
 		isInWord = false;
 		myWord = new char[5];
 		WordofDay = new char[5];
-		key = new char[5];
-		
+		key = new char[5];	
 	}
-
+	
 	public char[] Check$(String myWord, String WordofDay) {
 
 		for(int i = 0; i < myWord.length(); i++) {
 			this.myWord[i] = myWord.charAt(i);
 			this.WordofDay[i] = WordofDay.charAt(i);
 		}
-		
 		for(int i = 0; i < this.myWord.length; i++) {
 			
 			if(this.myWord[i] == this.WordofDay[i]) {
@@ -30,20 +28,17 @@ public class CheckWord {
 				key[i] = 'X';
 			}
 		}
-		
 		for(int i = 0; i < this.myWord.length; i++) {
 			for(int j = 0; j < this.myWord.length; j++) {
 				if(this.WordofDay[i] == this.myWord[j] && i != j && this.WordofDay[i] != this.WordofDay[j]) {
 					key[j] = '#';
 					break;
 				}
-				
+				else if (i==j && this.WordofDay[i] == this.myWord[j] ) {
+					break;
+				}
 			}
-		}
-		
-		
+		}	
 		return key;
-
 	}
-
 }
